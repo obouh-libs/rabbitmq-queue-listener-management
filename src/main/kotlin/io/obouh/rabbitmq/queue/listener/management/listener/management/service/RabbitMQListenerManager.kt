958d39b40963016b160e00d0fd20624b816467ef
oualidbouh <bouh.oualid@gmail.com>
2022-01-14 22:26:34 +0100

    (FEAT) Add unit tests for service layer

---
+++ src/main/kotlin/io/obouh/rabbitmq/queue/listener/management/listener/management/service/RabbitMQListenerManager.kt
@@ -0,0 +1,41 @@
+package io.obouh.rabbitmq.queue.listener.management.listener.management.service
+
+import io.obouh.rabbitmq.queue.listener.management.listener.management.service.dto.RabbitMQListener
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistry
+import org.springframework.stereotype.Service
+
+@Service
+class RabbitMQListenerManager(val rabbitListenerEndpointRegistry: RabbitListenerEndpointRegistry) {
+
+    fun stop(id : String){
+       val listenerContainer : MessageListenerContainer = rabbitListenerEndpointRegistry.getListenerContainer(id);
+        if(listenerContainer != null){
+            listenerContainer.stop()
+        }
+    }
+
+    fun start(id : String){
+        val listenerContainer : MessageListenerContainer = rabbitListenerEndpointRegistry.getListenerContainer(id)
+        if(listenerContainer != null){
+            listenerContainer.start()
+        }
+    }
+
+    fun stopAll() {
+        rabbitListenerEndpointRegistry.stop();
+    }
+
+    fun startAll() {
+        rabbitListenerEndpointRegistry.start();
+    }
+
+    fun fetchAllListenersState():Set<RabbitMQListener>{
+        val rabbitMQListeners = HashSet<RabbitMQListener>()
+        rabbitListenerEndpointRegistry.listenerContainerIds.stream().forEach { listenerId ->
+            val rabbitMQListener = RabbitMQListener(listenerId,rabbitListenerEndpointRegistry.getListenerContainer(listenerId).isRunning)
+            rabbitMQListeners.add(rabbitMQListener)
+        }
+        return rabbitMQListeners;
+    }
+}
---
+++ src/test/kotlin/io/obouh/rabbitmq/queue/listener/management/listener/management/service/RabbitMQListenerManagerTest.kt
@@ -0,0 +1,89 @@
+package io.obouh.rabbitmq.queue.listener.management.listener.management.service
+
+import io.obouh.rabbitmq.queue.listener.management.listener.management.service.dto.RabbitMQListener
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistry
+
+@ExtendWith (MockitoExtension::class)
+class RabbitMQListenerManagerTest {
+
+    @Mock
+    lateinit var rabbitListenerEndpointRegistry:RabbitListenerEndpointRegistry
+
+    @InjectMocks
+    lateinit var rabbitMQListenerManager: RabbitMQListenerManager
+
+    @Test
+    fun should_stop_rabbitmq_listener_by_id (){
+        // Given
+        val id = "listener_id"
+        val messageListenerContainer:MessageListenerContainer = mock(MessageListenerContainer::class.java)
+        `when`(rabbitListenerEndpointRegistry.getListenerContainer(eq(id))).thenReturn(messageListenerContainer)
+
+        // When
+        rabbitMQListenerManager.stop(id)
+
+        // Then
+        verify(messageListenerContainer, times(1)).stop()
+    }
+
+    @Test
+    fun should_start_rabbitmq_listener_by_id (){
+        // Given
+        val id = "listener_id"
+        val messageListenerContainer:MessageListenerContainer = mock(MessageListenerContainer::class.java)
+        `when`(rabbitListenerEndpointRegistry.getListenerContainer(eq(id))).thenReturn(messageListenerContainer)
+
+        // When
+        rabbitMQListenerManager.start(id)
+
+        // Then
+        verify(messageListenerContainer, times(1)).start()
+    }
+
+    @Test
+    fun should_start_all_rabbitmq_listeners (){
+        // Given
+
+        // When
+        rabbitMQListenerManager.startAll()
+
+        // Then
+        verify(rabbitListenerEndpointRegistry, times(1)).start()
+    }
+
+    @Test
+    fun should_stop_all_rabbitmq_listeners (){
+        // Given
+
+        // When
+        rabbitMQListenerManager.stopAll()
+
+        // Then
+        verify(rabbitListenerEndpointRegistry, times(1)).stop()
+    }
+
+    @Test
+    fun should_fetch_rabbitmq_listener_stats (){
+        // Given
+        val id = "listener_id"
+        val messageListenerContainer:MessageListenerContainer = mock(MessageListenerContainer::class.java)
+        `when`(rabbitListenerEndpointRegistry.listenerContainerIds).thenReturn(setOf(id))
+        `when`(rabbitListenerEndpointRegistry.getListenerContainer(id)).thenReturn(messageListenerContainer)
+        `when`(messageListenerContainer.isRunning).thenReturn(true)
+
+        // When
+        val rabbitMQListeners:HashSet<RabbitMQListener> = rabbitMQListenerManager.fetchAllListenersState() as HashSet<RabbitMQListener>
+
+        // Then
+        assertThat(rabbitMQListeners.iterator().next().id).isEqualTo(id)
+        assertThat(rabbitMQListeners.iterator().next().running).isTrue
+    }
+}
