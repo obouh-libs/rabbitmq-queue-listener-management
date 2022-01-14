8e1898eba2f84996e07f8e0bdf10abd29729a38b
oualidbouh <bouh.oualid@gmail.com>
2022-01-14 21:27:30 +0100

    (FEAT) Add Rest APIs + Service to start/stop rabbitmq spring listeners && fetch listeners status

---
+++ src/main/kotlin/io/obouh/rabbitmq/queue/listener/management/listener/management/rest/RabbitMQListenerController.kt
@@ -0,0 +1,25 @@
+package io.obouh.rabbitmq.queue.listener.management.listener.management.rest
+
+import io.obouh.rabbitmq.queue.listener.management.listener.management.service.dto.RabbitMQListener
+import io.obouh.rabbitmq.queue.listener.management.listener.management.service.dto.RabbitMQListenerManager
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/rabbitmq/listeners")
+class RabbitMQListenerController(val rabbitMQListenerManager: RabbitMQListenerManager) {
+
+    @PostMapping("/{id}/stop")
+    fun stop(@PathVariable id:String) = rabbitMQListenerManager.stop(id)
+
+    @PostMapping("/{id}/start")
+    fun start(@PathVariable id:String) = rabbitMQListenerManager.start(id)
+
+    @PostMapping("/stop")
+    fun stopAll() = rabbitMQListenerManager.stopAll()
+
+    @PostMapping("/start")
+    fun startAll() = rabbitMQListenerManager.startAll()
+
+    @GetMapping("/stats")
+    fun fetchListenersStates():Set<RabbitMQListener> = rabbitMQListenerManager.fetchAllListenersState()
+}
---
+++ src/main/kotlin/io/obouh/rabbitmq/queue/listener/management/listener/management/service/dto/RabbitMQListener.kt
@@ -0,0 +1,3 @@
+package io.obouh.rabbitmq.queue.listener.management.listener.management.service.dto
+
+data class RabbitMQListener(val id:String, var running:Boolean)
