de6b9408c860ffffee65e318dc5a49837b2e66d0
oualidbouh <bouh.oualid@gmail.com>
2022-01-14 21:29:21 +0100

    (FEAT) Add Library Configuration class && spring.factories file

---
+++ src/main/kotlin/io/obouh/rabbitmq/queue/listener/management/listener/management/configuration/RabbitMQListenersConfiguration.kt
@@ -0,0 +1,22 @@
+package io.obouh.rabbitmq.queue.listener.management.listener.management.configuration
+
+import io.obouh.rabbitmq.queue.listener.management.listener.management.rest.RabbitMQListenerController
+import io.obouh.rabbitmq.queue.listener.management.listener.management.service.dto.RabbitMQListenerManager
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistry
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class RabbitMQListenersConfiguration(@Autowired val rabbitListenerEndpointRegistry: RabbitListenerEndpointRegistry) {
+
+    @Bean
+    fun rabbitMQListenerManager():RabbitMQListenerManager {
+        return RabbitMQListenerManager(rabbitListenerEndpointRegistry);
+    }
+
+    @Bean
+    fun rabbitMQListenerController():RabbitMQListenerController {
+        return RabbitMQListenerController(rabbitMQListenerManager())
+    }
+}
